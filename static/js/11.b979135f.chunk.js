73ebdd2f22ac94c0f5bb59fabde942cd5b7ff7fb
Automated <actions@users.noreply.github.com>
2022-03-07 23:25:21 +0000

    Updates

--- static/js/11.b979135f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppersonal-site"]=this["webpackJsonppersonal-site"]||[]).push([[11],{174:function(e,s,n){"use strict";n.r(s);n(0);var t=n(25),a=n(1);s.default=function(){return Object(a.jsx)(t.a,{description:"JohnAnge Kernodle's personal website. DC based NC State graduate, and Andrews Accelerator Alumni."})}}}]);
-//# sourceMappingURL=11.b979135f.chunk.js.map
